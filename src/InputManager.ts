6362dc252e0306b3de3e9884c312f5e3c9cfb951
Matt Lander <matt@ciderpunk.net>
2023-04-29 14:29:05 +0100

    gamepad nightmare

--- src/InputManager.ts
+++ src/InputManager.ts
@@ -1,41 +1,34 @@
-import { GamepadManager, GenericPad } from "@babylonjs/core/Gamepads";
+
+import "@babylonjs/core/Gamepads/gamepadSceneComponent";
+import { GamepadManager, Xbox360Pad } from "@babylonjs/core/Gamepads";
 import { IGame, IInputManager } from "./interfaces";
 import { Vector2 } from "@babylonjs/core/Maths/math";
 
-
 export class InputManager implements IInputManager {
-  gpm: GamepadManager;
+  gpm: GamepadManager
 
-  public move = new Vector2(0, 0);
-  public fire = false;
-
-  //sourceMan: DeviceSourceManager
   public constructor(owner: IGame) {
     //joypad
     this.gpm = new GamepadManager(owner.scene);
     this.gpm.onGamepadConnectedObservable.add((gp, state) => {
-      
-      
-      console.log("Gamepad connected")
+      console.log(`gamepad connected ${gp.id}`)
+      if (gp instanceof Xbox360Pad){
+        const xpad = gp as Xbox360Pad
+        xpad.onleftstickchanged((values)=>{ 
+          console.log(`gamepad lstick move ${values.x}, ${values.y}`)
+          this.move.set( values.x, values.y )
+        })
+        xpad.onbuttondown((button)=>{
+          console.log(`gamepad button ${button}`)
+        })
+      }
+    })
 
-      const gamepad = gp as GenericPad;
-      gamepad.onButtonDownObservable.add((button, state) => { this.fire = true; console.log("fire"); });
-      gamepad.onButtonUpObservable.add((button, state) => { this.fire = false; console.log("no fire"); });
-
-      gamepad.onleftstickchanged((values) => {
-        this.move.set(values.x, values.y);
-
-
-        console.log(`joystick move ${values}`); 
-      });
-    });
-
-
-
-
-
-
-
+    this.gpm.onGamepadDisconnectedObservable.add((gp, state)=>{
+      console.log(`gamepad disconnected ${gp.id}`)
+    })
   }
 
+  move = new  Vector2(0,0)
+  fire:boolean = false
 }
--- src/entities/player.ts
+++ src/entities/player.ts
@@ -4,8 +4,29 @@ import { Person } from "./person";
 
 export class Player extends Person{
 
-  public constructor(name:string, owner:IGame, input:IInputManager){
+  public constructor(name:string, owner:IGame, readonly input:IInputManager){
     super(name, owner, { radiusTop:1, radiusBottom:1.6, height:6, start:new Vector3(0,5,0), capsuleBottom:-1.6, color:Color3.Green(), canterOfGravity:-3.5,offset:-3 , showForce:false })
+  }
+
+
+  
+  update(dT: number): void {
+
+    this.aliveTime+=dT
+
+    //console.log(this.input.move)
+
+
+    const force = new Vector3(30 * Math.sin(this.aliveTime * 0.0005),0,30* Math.cos(this.aliveTime  * 0.0005))
+    const point = this.forcePoint.getAbsolutePosition()
+
+    this.body.applyForce(force, point)
+    if (this.showForcePointer){
+    this.forcePointer.set(point,force)
+    }
+
+
+
   }
 
 }
